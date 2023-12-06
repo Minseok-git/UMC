09422932133bb5e729d84420d27c6fe5a0496354
minseok <cms8955@naver.com>
2023-12-06 17:38:07 +0900

    Mission7

---
+++ src/main/java/umc/study/domain/ReviewImage.java
@@ -0,0 +1,19 @@
+package umc.study.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class ReviewImage {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String imageUrl;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "review_id")
+    private Review review;
+}
